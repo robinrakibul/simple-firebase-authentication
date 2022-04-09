f7c57e458e99ece12031a746cb19804ad47a579f
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-09 15:29:58 +0600

    signin and signout implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
 import './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 function App() {
+  const [user,setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth,provider)
     .then(result=>{
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error =>{
       console.log('error',error);
     })
   }
+
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then( () =>{
+      setUser({})
+    })
+    .catch(error =>{
+      setUser({});
+    })
+  }
   return (
     <div className="App">
-     <button onClick={handleGoogleSignIn}>Google Sign-in</button>
+    {
+      // checking if user is signed in or not ternery condition
+      user.email?  <button onClick={handleSignOut}>Sign Out</button> :      
+      <button onClick={handleGoogleSignIn}>Google Sign-in</button>
+    }
+     <h2>Name: {user.displayName}</h2>
+     <h3>Your Email Address: {user.email}</h3>
+     <img src={user.photoURL} alt="" />
     </div>
   );
 }
