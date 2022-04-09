d15dd8391b8aa3c6bb88748b59a7f47d2e29e932
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-09 15:12:08 +0600

    added google signin authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import './firebase.init';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import app from './firebase.init';
 
 const auth = getAuth(app);
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () =>{
+    signInWithPopup(auth,provider)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error =>{
+      console.log('error',error);
+    })
+  }
   return (
     <div className="App">
-     <button>Google Sign-in</button>
+     <button onClick={handleGoogleSignIn}>Google Sign-in</button>
     </div>
   );
 }
