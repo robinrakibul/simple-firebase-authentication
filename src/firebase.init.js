7a4a81cc068f6dbaf5d5c7da0c8aa1b346df0559
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-09 14:54:42 +0600

    created firebase project, added firebase init js, empty button created

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB72usVsQ-IqEttsX2ZHbmQ-0-gUmGH7nc",
+  authDomain: "simple-firebase-authenti-f1455.firebaseapp.com",
+  projectId: "simple-firebase-authenti-f1455",
+  storageBucket: "simple-firebase-authenti-f1455.appspot.com",
+  messagingSenderId: "559873496497",
+  appId: "1:559873496497:web:8ef9880cc79a7c1d8a9990",
+  measurementId: "G-907QBSM415"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
